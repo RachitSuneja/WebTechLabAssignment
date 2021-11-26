b5a24cde6a9c2b9fbcef57c0663dc4c94fa1936b
RachitSuneja <81514106+RachitSuneja@users.noreply.github.com>
2021-11-26 19:40:02 +0530

    Add files via upload

---
+++ indexJs.js
@@ -0,0 +1,37 @@
+// Creating object 
+let Person1 = {
+
+    FirstName : "Ram",
+    LastName : "Singh",
+    OfficeAddress : "1st Floor,Mittal Heights,Rajpath Nagar, Patna"
+};
+
+/* Another Method of making objects
+let Person = new Object();
+person1.FirstName = "";
+person1.LastName = "";
+person1.OfficeAddress = "";
+
+*/
+console.log("Printing 1st Person ");
+console.log(Person1);
+
+let Person2 = {
+
+    FirstName :"Shyam",
+    LastName : "Kumar",
+    OfficeAddress : "4th Floor ,Satnam Tower ,Chandni Chowk, Delhi"
+};
+
+console.log("Printing 2nd Person ");
+console.log(Person2);
+
+let Person3 = {
+
+    FirstName :"Virat",
+    LastName : "Patel",
+    OfficeAddress : "2nd Floor, Country Inn Tower, Shanti Nagar, Indore"
+};
+
+console.log("Printing 3rd Person ");
+console.log(Person3);
